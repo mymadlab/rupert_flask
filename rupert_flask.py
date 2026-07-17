9b35df91bec003e0dfea7d4e6ff173c67fb0ebef
Bryce Klimoski <bryce@mymadlab.net>
2026-07-16 17:00:28 -0700

    Fix hello

--- rupert_flask.py
+++ rupert_flask.py
@@ -105,7 +105,7 @@ def event(event_type: str, name: str, event_profile: str) -> str:
 
 @beartype
 @app.route("/hello")
-def event(event_type: str, name: str, event_profile: str) -> str:
+def event()-> str:
 	"""
 	Hello check to confirm the API is working.
 	"""
